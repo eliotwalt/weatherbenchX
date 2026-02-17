d07627e277e82d416be06316cbe3bf4661452692
eliotwalt <walt.eliot@hotmail.com>
2026-02-17 19:01:11 +0100

    full dask

--- jobs/ens_mean_vs_era5_public_dask.sh
+++ jobs/ens_mean_vs_era5_public_dask.sh
@@ -54,5 +54,7 @@ python public_benchmark/run_benchmark_evaluation.py \
     --lead_time_chunk_size=4 \
     --init_time_chunk_size=1 \
     --backend=dask \
-    --n_workers=$N_WORKERS \
-    --threads_per_worker=$THREADS_PER_WORKER
+    --num_workers=1 \
+    --threads_per_worker=1
+    # --n_workers=$N_WORKERS \
+    # --threads_per_worker=$THREADS_PER_WORKER
