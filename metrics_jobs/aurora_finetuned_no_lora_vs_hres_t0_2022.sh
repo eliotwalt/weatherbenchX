e00d8379a4029dcf30559b1ce8856dcb348f91e7
eliotwalt <walt.eliot@hotmail.com>
2026-02-20 16:26:33 +0100

    all metrics jobs

--- metrics_jobs/aurora_finetuned_no_lora_vs_hres_t0_2022.sh
+++ metrics_jobs/aurora_finetuned_no_lora_vs_hres_t0_2022.sh
@@ -1,5 +1,11 @@
+# sbatch ./metrics_jobs/metrics.sh \
+#     --prediction=aurora_finetuned_no_lora_hres_init \
+#     --target=hres_t0 \
+#     --year=2022 \
+#     --output_dir=/projects/prjs1808/ewalt1/Xaurora/train/large-flow-map_600K_0.1-noise/forecast_hres_init_2022_15d_20-SDE-steps/
+
 sbatch ./metrics_jobs/metrics.sh \
-    --prediction=aurora_finetuned_no_lora_hres_init \
-    --target=hres_t0 \
-    --year=2022 \
-    --output_dir=/projects/prjs1808/ewalt1/Xaurora/train/large-flow-map_600K_0.1-noise/forecast_hres_init_2022_15d_20-SDE-steps/
+    --prediction=aurora_pretrained \
+    --target=era5 \
+    --year=2021 \
+    --output_dir=/projects/prjs1808/ewalt1/Xaurora/train/large-flow-map_600K_0.1-noise/forecast_2021_15d_20-SDE-steps/
